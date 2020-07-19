c3b927f952183d5b6b4045351e2a12132d15727e
officialrarlab <rarlabdev@gmail.com>
2020-07-19 13:33:08 +0200

    :rocket: Rework 'listenTo' to expect a Listener instead.

--- src/main/kotlin/club/rarlab/classicplugin/extension/HandlerExtensions.kt
+++ src/main/kotlin/club/rarlab/classicplugin/extension/HandlerExtensions.kt
@@ -1,28 +1,9 @@
 package club.rarlab.classicplugin.extension
 
-import org.bukkit.event.Event
-import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.plugin.EventExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
- * Class to handle corresponding [Listener] in terms of [Event]s.
+ * Listen to a specific [Listener].
  */
-class ClassicListener(private val listener: Event.() -> Unit) : EventExecutor, Listener {
-    /**
-     * Execute an [Event] in the [Listener].
-     */
-    override fun execute(ignored: Listener, event: Event) = listener(event)
-}
-
-/**
- * Listen to a specific [Event].
- */
-@Suppress("UNCHECKED_CAST")
-inline fun <reified T: Event> JavaPlugin.listenTo(
-        priority: EventPriority = EventPriority.NORMAL,
-        noinline listener: T.() -> Unit
-) = ClassicListener(listener as Event.() -> Unit).also {
-    this.server.pluginManager.registerEvent(T::class.java, it, priority, it, this)
-}
+fun JavaPlugin.listenTo(listener: Listener) = this.server.pluginManager.registerEvents(listener, this)
