dae1baef168cc2811749e58bcad4c30de0cbe080
officialrarlab <rarlabdev@gmail.com>
2020-08-08 14:05:40 +0200

    :rocket: FakePlayer nametag issues.

--- src/main/kotlin/club/rarlab/classicplugin/nms/entity/FakePlayer.kt
+++ src/main/kotlin/club/rarlab/classicplugin/nms/entity/FakePlayer.kt
@@ -1,19 +1,23 @@
 package club.rarlab.classicplugin.nms.entity
 
 import club.rarlab.classicplugin.extension.*
+import club.rarlab.classicplugin.hologram.HologramLine
 import club.rarlab.classicplugin.nms.GlobalReflection.FetchType.*
+import club.rarlab.classicplugin.nms.GlobalReflection.VERSION_NUMBER
 import club.rarlab.classicplugin.nms.GlobalReflection.get
 import club.rarlab.classicplugin.nms.ReflectionHelper
 import club.rarlab.classicplugin.nms.ReflectionHelper.createPacket
 import club.rarlab.classicplugin.nms.ReflectionHelper.sendPacket
 import club.rarlab.classicplugin.task.schedule
-import com.google.common.base.Splitter
 import com.mojang.authlib.GameProfile
 import com.mojang.authlib.properties.Property
+import org.apache.commons.lang.RandomStringUtils
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.entity.Player
+import org.bukkit.scoreboard.NameTagVisibility
 import org.bukkit.scoreboard.Scoreboard
+import org.bukkit.scoreboard.Team
 import java.lang.reflect.Array
 import java.lang.reflect.Constructor
 import java.lang.reflect.Method
@@ -22,7 +26,7 @@ import java.util.*
 /**
  * [FakePlayer] used to create fake [Player]s.
  */
-class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
+class FakePlayer private constructor(val owner: UUID, nameTag: String) {
     /**
      * [Any] built EntityPlayer object.
      */
@@ -36,7 +40,12 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
     /**
      * [String] of the actual NPC name.
      */
-    private val middleName: String
+    private val preciseName: String
+
+    /**
+     * [HologramLine] of the [FakePlayer]'s nametag.
+     */
+    private val hologram: HologramLine
 
     /**
      * [Scoreboard] instance to be used for name tag.
@@ -92,6 +101,7 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
     fun setLocation(location: Location) {
         val (x, y, z, yaw, pitch) = location
         get<Method>(METHOD, "Entity_setLocation").invoke(this.entity, x, y, z, yaw, pitch)
+        hologram.updateLocation(location.clone().subtract(0.0, 0.1, 0.0)) // PERHAPS WE NEED TO PASS PLAYERS??!?!
     }
 
     /**
@@ -104,14 +114,16 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
      *
      * @param players array of [Player] to show to.
      */
-    fun showTo( vararg players: Player) {
+    fun showTo(vararg players: Player) {
         sendPacket(createPacket("PacketPlayOutPlayerInfo", getInfoAction("ADD_PLAYER"), this.entityArray), *players)
         sendPacket(createPacket("PacketPlayOutNamedEntitySpawn", this.entity), *players)
         equipment?.packets(getEntityId() ?: return)?.forEach { packet -> sendPacket(packet, *players) }
 
         players.forEach { player -> player.scoreboard = this.scoreboard }
-        sendPacket(createPacket("PacketPlayOutScoreboardTeam", this.scoreboardTeam, listOf(this.middleName), 3), *players)
-        schedule(20, false, Runnable {
+        sendPacket(createPacket("PacketPlayOutScoreboardTeam", this.scoreboardTeam, listOf(this.preciseName), 3), *players)
+        hologram.showTo(*players)
+
+        schedule(40, false, Runnable {
             sendPacket(createPacket("PacketPlayOutPlayerInfo", getInfoAction("REMOVE_PLAYER"), this.entityArray), *players)
         })
     }
@@ -123,9 +135,9 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
      */
     fun hideFrom(vararg players: Player) {
         val entityId = getEntityId() ?: return
-
         sendPacket(createPacket("PacketPlayOutPlayerInfo", getInfoAction("REMOVE_PLAYER"), this.entityArray), *players)
         sendPacket(createPacket("PacketPlayOutEntityDestroy", intArrayOf(entityId)), *players)
+        hologram.hideFrom(*players)
     }
 
     /**
@@ -152,7 +164,7 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
      *
      * @return [Player] corresponding object.
      */
-    private fun getPlayer(): Player? = Bukkit.getPlayer(this.uuid)
+    private fun getPlayer(): Player? = Bukkit.getPlayer(this.owner)
 
     /**
      * Pre initialization.
@@ -161,20 +173,21 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
         val player = getPlayer() ?: throw NullPointerException("Player is offline/invalid!")
         val worldServer = ReflectionHelper.getWorldServer(player)
 
-        val names = Splitter.fixedLength(16).split(nameTag).toList()
-        val prefix = names[0]
-        val name = names.getOrNull(1)
-        val suffix = names.getOrNull(2)
-        val preciseName = (name ?: prefix).also { this.middleName = it }
+        preciseName = RandomStringUtils.randomAlphanumeric(16)
+        hologram = HologramLine(player.location.clone().add(0.0, 0.15, 0.0), nameTag)
 
         get<Constructor<*>>(CONSTRUCTOR, "EntityPlayer").newInstance(
                 SERVER, worldServer, GameProfile(UUID.randomUUID(), preciseName),
                 get<Constructor<*>>(CONSTRUCTOR, "PlayerInteractManager").newInstance(worldServer)
         ).also { entity -> this.entity = entity }
 
-        val bukkitTeam = scoreboard.registerNewTeam("roam_${player.name}")
-        name?.let { bukkitTeam.prefix = prefix }
-        suffix?.let { bukkitTeam.suffix = it }
+        val bukkitTeam = scoreboard.registerNewTeam("roam_${player.name}").apply {
+            if (VERSION_NUMBER >= 91) {
+                this.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
+                return@apply
+            }
+            this.nameTagVisibility = NameTagVisibility.NEVER
+        }
 
         val nmsScoreboard = get<Constructor<*>>(CONSTRUCTOR, "Scoreboard").newInstance()
         this.scoreboardTeam = get<Constructor<*>>(CONSTRUCTOR, "ScoreboardTeam").newInstance(nmsScoreboard, bukkitTeam.name)
@@ -197,10 +210,10 @@ class FakePlayer private constructor(val uuid: UUID, nameTag: String) {
         /**
          * Generate a new [FakePlayer] by owner unique id and name tag.
          *
-         * @param uniqueId whom to be the owner of this [FakePlayer].
-         * @param nameTag  to be set for the [FakePlayer].
+         * @param owner   whom to be the owner of this [FakePlayer].
+         * @param nameTag to be set for the [FakePlayer].
          * @return [FakePlayer] corresponding fake player.
          */
-        fun generate(uniqueId: UUID, nameTag: String): FakePlayer = FakePlayer(uniqueId, nameTag)
+        fun generate(owner: UUID, nameTag: String): FakePlayer = FakePlayer(owner, nameTag)
     }
 }
